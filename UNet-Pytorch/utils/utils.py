af239cdd3526ec4db8c6b9635d235f0b388147b1
Hurwitzzz <75025768+Hurwitzzz@users.noreply.github.com>
2022-11-28 19:09:28 +0100

    checked the dataloader Show example data in dataset Stop when I found the dataset has problem Waiting for Burak to fix the dataset.

--- UNet-Pytorch/utils/utils.py
+++ UNet-Pytorch/utils/utils.py
@@ -15,3 +15,29 @@ def plot_img_and_mask(img, mask):
         ax[1].imshow(mask)
     plt.xticks([]), plt.yticks([])
     plt.show()
+
+
+def plot_example_imgs_from_dataset(dataset,num_example_imgs):
+
+    plt.figure(figsize=(20, 10 * num_example_imgs))
+    for i in range(num_example_imgs):
+        
+        img=dataset[i]['image']
+        mask=dataset[i]['mask']
+        print(i)
+        # sampled imgs
+        plt.subplot(num_example_imgs, 2, i * 2 + 1)
+        plt.imshow(img.numpy().transpose(1,2,0))
+        plt.title(dataset.GetFileName(i))
+        # plt.axis('off')
+        # if i == 0:
+        #     plt.title("Sampled image")
+        
+        # mask
+        plt.subplot(num_example_imgs, 2, i * 2 + 2)
+        plt.imshow(mask.numpy())
+        plt.title(dataset.GetFileName(i))
+        # plt.axis('off')
+        # if i == 0:
+        #     plt.title("Mask image")
+    plt.show()
